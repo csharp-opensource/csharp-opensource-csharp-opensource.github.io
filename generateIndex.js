329d5592136d5ecaf6da9d6dd420d833e0700454
Bar Nuri <barnuri@hotmail.com>
2024-12-05 12:10:11 +0200

    fix

--- generateIndex.js
+++ generateIndex.js
@@ -5,18 +5,26 @@ let headers = {
     'Authorization': process.env.GITHUB_TOKEN,
 };
 
-async function getRepo(url) {
+async function getRepos(url) {
     try {
         const response = await fetch(url, { headers });
         if (!response.ok) throw new Error("Failed to fetch repositories");
-        let repo = await response.json();
-        repo = {
+        const repos = await response.json();
+        return repos.map(repo => ({
             html_url: repo.html_url,
             name: repo.name,
             description: repo.description,
             contributors_url: repo.contributors_url,
             pushed_at: repo.pushed_at,
-        };
+        }));
+    } catch (error) {
+        console.error(`Error fetching repository ${url}`, error);
+        return [];
+    }
+}
+
+async function appendRepoContributers(repos) {
+    await Promise.all(repos.map(async (repo) => {
         if (repo.contributors_url) {
             try {
                 const contributorsResponse = await fetch(repo.contributors_url);
@@ -28,17 +36,13 @@ async function getRepo(url) {
             }
         }
         repo.contributorsList = repo.contributorsList || [];
-        return repo;
-    } catch (error) {
-        console.error(`Error fetching repository ${url}`, error);
-        return null;
-    }
+    }));
 }
 
 async function fetchRepositories() {
     try {
         const reposPromises = config.fetchConfigs.map(({ orgOrUser, isUser }) =>
-            getRepo(`https://api.github.com/${isUser ? "users" : "orgs"}/${orgOrUser}/repos?sort=pushed`)
+            getRepos(`https://api.github.com/${isUser ? "users" : "orgs"}/${orgOrUser}/repos?sort=pushed`)
         );
         const allRepos = (await Promise.all(reposPromises))
             .flat()
@@ -53,6 +57,7 @@ async function fetchRepositories() {
 
 async function modifyHTML() {
     const repos = await fetchRepositories();
+    await appendRepoContributers(repos);
     let indexContent = fs.readFileSync('./index.html', 'utf-8');
     indexContent = indexContent.replace(`SET_TITLE`, config.title);
     indexContent = indexContent.replace(`SET_REPO_JSON`, JSON.stringify(repos, undefined, 4));
